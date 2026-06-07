6091141eb88a860525b9d7ba1bf83779ec58a92d
UnrealNFT <beatcoin2023@gmail.com>
2026-06-07 11:35:15 +0200

    Fix CSPR.click app_id to use template instead of unauthorized custom id

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,6 +1,6 @@
 const config = {
   cspr_click_app_name: "TrappistAI",
-  cspr_click_app_id: "trappistai-platform",
+  cspr_click_app_id: "csprclick-template",
   cspr_click_providers: ['casper-wallet', 'ledger', 'metamask-snap'],
   cspr_live_url: "https://cspr.live",
 };
